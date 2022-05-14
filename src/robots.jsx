749cbf90d4b41a462af84bcba63edd8b105fcd09
Greg Wilson <gregwilson3468@gmail.com>
2022-05-13 20:51:00 -0400

    added Searchbox.js and worked on search box functionality.

---
+++ src/robots.jsx
@@ -0,0 +1,55 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Pankins',
+        email: 'pankins@poonch.biz'
+    },
+    {
+        id: 2,
+        name: 'Wez',
+        email: 'wez@deer.edu'
+    },
+    {
+        id: 3,
+        name: 'Butter boo',
+        email: 'liamisthecoolest2010@butterboo.io'
+    },
+    {
+        id: 4,
+        name: 'Fudge',
+        email: 'fudge@butterboo.io'
+    },
+    {
+        id: 5,
+        name: 'Bunny',
+        email: 'bunny@butterboo.io'
+    },
+    {
+        id: 6,
+        name: 'Raleigh',
+        email: 'raleigh@butterboo.io'
+    },
+    {
+        id: 7,
+        name: 'Daddy',
+        email: 'daddy@butterboo.io'
+    },
+    {
+        id: 8,
+        name: 'Gamma',
+        email: 'canthear@notitalian.it'
+    },
+    {
+        id: 9,
+        name: 'Coleman',
+        email: 'yesdear@raleigh.io'
+    },
+    {
+        id: 10,
+        name: 'Karen',
+        email: 'hatessteve@sales.io'
+    }
+
+
+];
+
